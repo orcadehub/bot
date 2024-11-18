5ae8a170ef5ce57b30bbe0c5c44564a92d672e03
orcadehub <orcadehub2@gmail.com>
2024-11-19 00:02:41 +0530

    Update user_model.js

--- models/user_model.js
+++ models/user_model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  fullName: { type: String, required: false, default: null },
+  // fullName: { type: String, required: false, default: null },
   // password: { type: String, required: false, default: null },
   // email: { type: String, required: false, unique: false },
   // mobileNumber: { type: String, required: false, unique: false },
